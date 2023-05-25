c98589a1e380838fbbc6c2cf9aa57ca1e4acc673
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-25 20:06:10 -0300

    correcion import

--- Coderhouse_40445/project/apps/home/forms.py
+++ Coderhouse_40445/project/apps/home/forms.py
@@ -1,19 +1,17 @@
 from django import forms
-
-from . import models
-from models import Perro,Gato, Exotico
+from .models import Perro, Gato, Exotico
 
 class PerroForm(forms.ModelForm):
     class Meta:
-        model = models.Perro
+        model = Perro
         fields = "__all__"
 
 class GatoForm(forms.ModelForm):
     class Meta:
-        model = models.Gato
+        model = Gato
         fields = "__all__"
 
 class ExoticoForm(forms.ModelForm):
     class Meta:
-        model = models.Exotico
+        model = Exotico
         fields = "__all__"        
