a8d6db61b30414e7bb1f69bb7486409eede1ce89
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-24 23:35:34 -0300

    agregar pérro

--- Coderhouse_40445/project/apps/home/views.py
+++ Coderhouse_40445/project/apps/home/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from . import forms
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "home/index.html")
+
+def agregar_perro(request):
+    form=forms.PerroForm()
+    return render(request, "home/agregar-perro.html")
+
+def agregar_gato(request):
+    form=forms.GatoForm()
+    return render(request, "home/agregar-gato.html")
+
+def agregar_perro(request):
+    form=forms.ExoticoForm()
+    return render(request, "home/agregar-exotico.html")
