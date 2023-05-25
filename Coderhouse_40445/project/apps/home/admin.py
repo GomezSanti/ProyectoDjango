3485602913e9f98409f602c1ecc122cae9356c7a
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-24 23:15:06 -0300

    admin

--- Coderhouse_40445/project/apps/home/admin.py
+++ Coderhouse_40445/project/apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Perro)
+admin.site.register(models.Gato)
+admin.site.register(models.Exotico)
