4a61b16eee76703ca327348b689d404d2ee76edd
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-25 19:26:25 -0300

    html gato exotico

--- Coderhouse_40445/project/apps/home/forms.py
+++ Coderhouse_40445/project/apps/home/forms.py
@@ -2,17 +2,17 @@ from django import forms
 
 from . import models
 
-class PerroForm(forms.modelform):
+class PerroForm(forms.ModelForm):
     class Meta:
         model = models.Perro
         fields = "__all__"
 
-class GatoForm(forms.modelform):
+class GatoForm(forms.ModelForm):
     class Meta:
         model = models.Gato
         fields = "__all__"
 
-class ExoticoForm(forms.modelform):
+class ExoticoForm(forms.ModelForm):
     class Meta:
         model = models.Exotico
         fields = "__all__"        
--- Coderhouse_40445/project/apps/home/views.py
+++ Coderhouse_40445/project/apps/home/views.py
@@ -1,18 +1,38 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from . import forms
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "home/index.html")
 
 def agregar_perro(request):
-    form=forms.PerroForm()
-    return render(request, "home/agregar-perro.html")
-
+    if request.method == "POST":
+         form = forms.PerroForm(request.POST)
+         if form.is_valid():
+             form.save()
+         return redirect("index")
+    else:
+        form= forms.PerroForm()
+    context={"form": form}
+    return render(request, "home/agregar-perro.html", context)
 def agregar_gato(request):
-    form=forms.GatoForm()
-    return render(request, "home/agregar-gato.html")
+    if request.method == "POST":
+         form = forms.GatoForm(request.POST)
+         if form.is_valid():
+             form.save()
+         return redirect("index")
+    else:
+        form= forms.GatoForm()
+    context={"form": form}
+    return render(request, "home/agregar-gato.html", context)
 
-def agregar_perro(request):
-    form=forms.ExoticoForm()
-    return render(request, "home/agregar-exotico.html")
+def agregar_exotico(request):
+    if request.method == "POST":
+         form = forms.ExoticoForm(request.POST)
+         if form.is_valid():
+             form.save()
+         return redirect("index")
+    else:
+        form= forms.ExoticoForm()
+    context={"form": form}
+    return render(request, "home/agregar-Exotico.html", context)
