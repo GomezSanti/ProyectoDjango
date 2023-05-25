1da5d9ced86fd38f1ada4d978e120c56b24cdc73
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-24 23:06:28 -0300

    self

--- Coderhouse_40445/project/apps/home/models.py
+++ Coderhouse_40445/project/apps/home/models.py
@@ -7,12 +7,21 @@ class Perro(models.Model):
     edad = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.raza  
+
 class Gato(models.Model):
     raza = models.CharField(max_length=100)
     edad = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.raza  
+    
 class Exotico(models.Model):
     raza = models.CharField(max_length=100)
     edad = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.raza  
