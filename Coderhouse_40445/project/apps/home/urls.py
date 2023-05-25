8751b01a4d8ec2e751b8e5417aa5f31a7053f6ab
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-24 23:23:18 -0300

    parentesis

--- Coderhouse_40445/project/apps/home/urls.py
+++ Coderhouse_40445/project/apps/home/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-]
+    path("Agregar-Perro/", views.agregar_perro, name="agregar-perro"),
+    path("Agregar-Gato/", views.agregar_gato, name="agregar-gato"),
+    path("Agregar-Exotico/", views.agregar_exotico, name="agregar-exotico"),
+    ]
 urlpatterns += staticfiles_urlpatterns()
