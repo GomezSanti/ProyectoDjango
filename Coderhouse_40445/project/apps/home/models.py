1d7e5e9f2ec597bb11a4ff3ef276691e768f8e37
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-24 22:53:39 -0300

    modelos

--- Coderhouse_40445/project/apps/home/models.py
+++ Coderhouse_40445/project/apps/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Perro(models.Model):
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Gato(models.Model):
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Exotico(models.Model):
+    raza = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    peso = models.DecimalField(max_digits=5, decimal_places=2)
